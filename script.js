8bc47d16d79c065989c56bf9a99ff4bcfdc62041
Vandan Bhangale <159510950+Vandan-Bhangale@users.noreply.github.com>
2025-02-04 23:10:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ more.addEventListener("click",() => {
     loadContent(1);
 })
 
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+setInterval(nextSlide, 5000);
